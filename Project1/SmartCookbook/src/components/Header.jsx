87b75fb3c6958fa5e450ab955e54d62cc68f4b3d
UC-KBish <bishopkw@mail.uc.edu>
2023-10-10 00:14:04 -0400

    About to move hamburger menu to component

--- Project1/SmartCookbook/src/components/Header.jsx
+++ Project1/SmartCookbook/src/components/Header.jsx
@@ -1,52 +1,51 @@
 import React, { useState } from 'react';
 
+import HamburgerMenu from './HamburgerMenu';
+
 function Header(props) {
 
-    const [inputValue, setInputValue] = useState("");
+    const handleInputChange = () => {
+        const menu = document.getElementById('menu');
+        const menu_button = document.getElementById('menu-button');
+        const menu_button_back = document.getElementById('menu-button-back');
 
-    const menu = document.getElementById('menu');
-
-    const handleInputChange = (event) => {
-        const newValue = event.target.value;
-        setInputValue(newValue);
-
-        if (menu.style.right === '-250px') {
-            menu.style.right = '0';
+        if (menu.style.right === '-50vw') {
+            menu.style.right = '-100vw';
+            menu_button.style.right = '0.5rem';
+            menu_button_back.style.right = '-2rem';
         } else {
-            menu.style.right = '-250px';
+            menu.style.right = '-50vw';
+            menu_button.style.right = '2.5rem';
+            menu_button_back.style.right = '0.5rem';
         }
-
-
     };
 
+    let title = <h1 style={{ marginLeft: '10px', fontSize: '200%' }}>{props.title}</h1>;
 
     if (props.backToIndex) {
-        return (<>
-            <div className='flex' style={{ minWidth: '100%', height: '60px', backgroundColor: '#c0c0c0', top: '0', position: 'absolute', justifyContent: 'center' }} />
-            <div className='flexVerticallyAlligned'>
-                <a href='/' style={{ textDecoration: 'none', 'color': 'black' }}>
-                    <h1 style={{ marginLeft: '10px', fontSize: '200%' }}>{props.title}</h1>
-                </a>
-                <img src='HamburgerMenu.png' style={{ maxWidth: '60%', maxHeight: '60%', marginRight: '20px' }}></img>
-            </div>
-        </>);
+        title = <a href='/' style={{ textDecoration: 'none', 'color': 'black' }}>{title}</a>
     }
-    return (<>
-        <div className='flex' style={{ minWidth: '100%', height: '60px', backgroundColor: '#c0c0c0', top: '0', position: 'absolute', justifyContent: 'center' }} />
-        <div className='flexVerticallyAlligned'>
-            <h1 style={{ marginLeft: '10px', fontSize: '200%' }}>{props.title}</h1>
+
+    return (
+        <header>
+            {title}
             <div id="menu" class="hidden">
                 <ul>
-                    <li><a href="#">Item 1</a></li>
-                    <li><a href="#">Item 2</a></li>
-                    <li><a href="#">Item 3</a></li>
+                    <li><a href="#">Profile</a></li>
+                    <li><a href="/">Search</a></li>
+                    <li><a href="#">Saved</a></li>
                 </ul>
             </div>
             <button id="menu-button" onClick={handleInputChange}>
-                <img src='HamburgerMenu.png' style={{ maxWidth: '60%', maxHeight: '60%', marginRight: '20px' }} />
+                <img src='HamburgerMenu.png' />
             </button>
-        </div>
-    </>);
+            <button id="menu-button-back" onClick={handleInputChange}>
+                <img src='ArrowBack.png' />
+            </button>
+
+        </header>
+    );
+
 }
 
 export default Header
